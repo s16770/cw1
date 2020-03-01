a636e770ddefc2f0fe3924f33d58de03c4cdddab
Bartłomiej Kisil <s16770@pjwstk.edu.pl>
2020-03-01 18:07:48 +0100

    cw1zad2

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace cw1
@@ -14,9 +15,17 @@ namespace cw1
             if (result.IsSuccessStatusCode) //prawidlowy czyli 2xx
             {
                 string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z0-9]+@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+
+                MatchCollection matches = regex.Matches(html);
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m);
+                }
             }
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Koniec!");
         }
     }
 }
