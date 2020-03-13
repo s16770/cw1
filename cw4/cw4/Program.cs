6f7638d612aff043934adb07e1f5729253b0f66b
Bartłomiej Kisil <s16770@pjwstk.edu.pl>
2020-03-13 22:23:05 +0100

    update zadań z I zjazdu - wszystko gotowe

--- cw4/cw4/Program.cs
+++ cw4/cw4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -12,25 +13,49 @@ namespace cw1
 
             if (args.Length > 0) //czy podany argument - adres url
             {
-
                 if (Uri.IsWellFormedUriString(args[0], UriKind.Absolute)) //czy podany argument/adres jest poprawny
                 {
-                    var client = new HttpClient();
-
-                    var result = await client.GetAsync(args[0]);
-
-                    if (result.IsSuccessStatusCode) //prawidlowy czyli 2xx
+                    try
                     {
-                        string html = await result.Content.ReadAsStringAsync();
-                        var regex = new Regex("[a-z0-9]+@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                        var client = new HttpClient();
 
+                        var result = await client.GetAsync(args[0]);
+                        client.Dispose();
 
-                        MatchCollection matches = regex.Matches(html);
-                        foreach (var m in matches)
+                        if (result.IsSuccessStatusCode) //prawidlowy czyli 2xx
                         {
-                            Console.WriteLine(m);
-                        }
+                            string html = await result.Content.ReadAsStringAsync();
+                            var regex = new Regex("[a-z0-9]+@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                            int licznik = 0;
+
+                            MatchCollection matches = regex.Matches(html);
+
+                            Hashtable uniMatches = new Hashtable();
+
+                            foreach (var m in matches)
+                            {
+                                licznik++;
+                                string uniMatch = m.ToString();
+                                if (!uniMatches.Contains(uniMatch))
+                                    uniMatches.Add(uniMatch, string.Empty);
+                            }
+                            
+                            foreach (DictionaryEntry element in uniMatches)
+                            {
+                                Console.WriteLine(element.Key);
+                            }
+                                
+                            if (licznik == 0)
+                            {
+                                Console.WriteLine("Nie znaleziono adresow email.");
+                            }
+                        }  
                     }
+                    catch
+                    {
+                        Console.WriteLine("Wystapil blad podczas ladowania strony");
+                    }
+                    
                 }
                 else
                 {
