a441f32c375509ec90281a08a47989947b024d26
Bartłomiej Kisil <s16770@pjwstk.edu.pl>
2020-03-13 21:04:09 +0100

    zadanie 4

---
+++ cw4/cw4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cw1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+
+            if (args.Length > 0) //czy podany argument - adres url
+            {
+
+                if (Uri.IsWellFormedUriString(args[0], UriKind.Absolute)) //czy podany argument/adres jest poprawny
+                {
+                    var client = new HttpClient();
+
+                    var result = await client.GetAsync(args[0]);
+
+                    if (result.IsSuccessStatusCode) //prawidlowy czyli 2xx
+                    {
+                        string html = await result.Content.ReadAsStringAsync();
+                        var regex = new Regex("[a-z0-9]+@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+
+                        MatchCollection matches = regex.Matches(html);
+                        foreach (var m in matches)
+                        {
+                            Console.WriteLine(m);
+                        }
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Podano zly adres!!");
+                }
+
+            }
+            else
+            {
+                throw new ArgumentNullException("Nie podano adresu URL!!");
+            }
+
+
+
+            Console.WriteLine("Koniec!");
+        }
+    }
+}
